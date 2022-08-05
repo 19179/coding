d7655942334dc04886fc9c3ba269e69adbec6615
Sophie Elliott <82548883+19179@users.noreply.github.com>
2022-08-05 00:03:59 +0000

    got ticket loop sorted out and number checker part done i believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import statements
-import ticket_loop_v1
+import number_checker
+
 # funtions go here
 
 
@@ -23,7 +24,36 @@ def not_blank(question):
 # Ask user if they have used the program before & show instructions if necessary
 
 # Loop to get ticket details 
+			
+# start of loop 
 
+# initialise loop so that it runs at least once
+name = ""
+count = 0
+MAX_TICKETS = 5 
+
+while name != "xxx" and count < MAX_TICKETS:
+	
+	# tells user how many seats are left
+	if count < 4:
+		print("You have {} seats "
+			"left".format(MAX_TICKETS - count))
+	
+		# Warns user that only one seat is left!
+	else:
+		print("*** There is ONE seat left!! ***")
+	
+	# Gets details...
+	name = input("Name: ")
+	count += 1
+
+
+if count == MAX_TICKETS:
+	print("You have sold all the avaiable tickets!")
+else:
+	print("You have sold {} tickets. \n"
+			 "There are {} places still available"
+			 .format (count, MAX_TICKETS - count))
 	# Get name (can't be blank)
 	name = not_blank("Name: ")
 	# Get age (between 12 and 130)
